e39b26f723f6ca900f66f0cbf8e62cb2256ab0e2
Enrico Emiro <16721212+enricoemiro@users.noreply.github.com>
2022-07-24 17:01:11 +0200

    chore: rename REQUIRES_NOT_ON_SELF_KEY value

--- src/auth/auth.decorator.ts
+++ src/auth/auth.decorator.ts
@@ -1,7 +1,7 @@
 import { SetMetadata } from '@nestjs/common';
 
 export const REQUIRES_AUTH_KEY = 'requiresAuth';
-export const REQUIRES_NOT_ON_SELF_KEY = 'notOnSelf';
+export const REQUIRES_NOT_ON_SELF_KEY = 'requiresNotOnSelf';
 
 export const RequiresAuth = (requiresAuth: boolean) =>
   SetMetadata(REQUIRES_AUTH_KEY, requiresAuth);
