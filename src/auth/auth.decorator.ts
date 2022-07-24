98c15a0abc4b3c384e02d70139df75e0ddc1a065
Enrico Emiro <16721212+enricoemiro@users.noreply.github.com>
2022-07-24 16:37:04 +0200

    chore: rename REQUIRES_AUTH_KEY value

--- src/auth/auth.decorator.ts
+++ src/auth/auth.decorator.ts
@@ -1,6 +1,6 @@
 import { SetMetadata } from '@nestjs/common';
 
-export const REQUIRES_AUTH_KEY = 'requires_auth';
+export const REQUIRES_AUTH_KEY = 'requiresAuth';
 
 export const RequiresAuth = (requiresAuth: boolean) =>
   SetMetadata(REQUIRES_AUTH_KEY, requiresAuth);
