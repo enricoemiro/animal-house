147564e97e3f3ee8aece7f1363e5529dfc935c7c
Enrico Emiro <16721212+enricoemiro@users.noreply.github.com>
2022-07-28 21:30:25 +0200

    chore: add image (module, service, interface and validator)

---
+++ src/image/image.interface.ts
@@ -0,0 +1,19 @@
+import { Types } from 'mongoose';
+
+import { FileOptions } from '@app/file/file.interface';
+
+export interface ImageOptions extends Pick<FileOptions, 'buffer'> {
+  /**
+   * Image owner id.
+   */
+  ownerId: Types.ObjectId;
+
+  /**
+   * Image type.
+   */
+  type: ImageType;
+}
+
+export enum ImageType {
+  USER = 'user',
+}
---
+++ src/image/image.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import sharp, { ResizeOptions } from 'sharp';
+
+import { FileService } from '@app/file/file.service';
+
+import { ImageOptions, ImageType } from './image.interface';
+
+@Injectable()
+export class ImageService {
+  public constructor(private fileService: FileService) {}
+
+  public async create(imageOptions: ImageOptions) {
+    const webpBuffer = await this.toWebp(imageOptions.buffer);
+
+    return this.fileService.create({
+      path: `${this.buildPath(imageOptions)}.webp`,
+      buffer: webpBuffer,
+    });
+  }
+
+  /**
+   * Return the file path.
+   *
+   * @param fileOptions File options.
+   * @returns the file path.
+   */
+  private buildPath({ ownerId, type }: ImageOptions) {
+    switch (type) {
+      case ImageType.USER:
+        return `users/${ownerId}`;
+    }
+  }
+
+  /**
+   * Convert an image to webp format.
+   *
+   * @param image Image buffer.
+   * @param resizeOptions Sharp resize options (optional).
+   * @returns Webp image buffer.
+   */
+  private async toWebp(
+    image: Buffer,
+    resizeOptions: ResizeOptions = { width: 512, height: 512 },
+  ) {
+    try {
+      return await sharp(image).resize(resizeOptions).webp().toBuffer();
+    } catch (error: any) {
+      throw error;
+    }
+  }
+}
