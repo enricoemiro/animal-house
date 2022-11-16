926b1dd2a7f0fb253c51bcef89eff097116eee7e
Giulio Billi <giulio.billi2@studio.unibo.it>
2022-11-16 17:18:51 +0100

    chore: add login signup route

--- game/src/routes.js
+++ game/src/routes.js
@@ -1,5 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import Login from '@app/auth/login/Login';
+import Base from '@app/auth/shared/Base';
+import Signup from '@app/auth/signup/Signup';
 import Home from '@app/home/Home';
 import Memory from '@app/memory/Memory';
 
@@ -11,6 +14,14 @@ const router = createBrowserRouter([
   {
     path: '/memory',
     element: <Memory />,
+  },
+  {
+    path: '/auth',
+    element: <Base />,
+    children: [
+      { path: 'login', element: <Login />, index: true },
+      { path: 'signup', element: <Signup />, index: true },
+    ],
   },
 ]);
 
