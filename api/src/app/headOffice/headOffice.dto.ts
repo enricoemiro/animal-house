06a7f918e124b01d2593aedfb546334ef158e414
Enrico Emiro <16721212+enricoemiro@users.noreply.github.com>
2023-02-10 15:24:23 +0100

    feat(api): prevent xss attack

--- api/src/app/headOffice/headOffice.dto.ts
+++ api/src/app/headOffice/headOffice.dto.ts
@@ -1,13 +1,17 @@
+import { Transform } from 'class-transformer';
 import { IsLatLong, IsMongoId, IsString } from 'class-validator';
+import xss from 'xss';
 
 export class HeadOfficeDTO {
   @IsMongoId()
   id: string;
 
   @IsString()
+  @Transform(({ value }) => xss(value))
   location: string;
 
   @IsString()
+  @Transform(({ value }) => xss(value))
   streetAddress: string;
 
   @IsLatLong()
--- api/src/app/post/post.dto.ts
+++ api/src/app/post/post.dto.ts
@@ -1,11 +1,14 @@
 import { PostCategory } from '@prisma/client';
+import { Transform } from 'class-transformer';
 import { IsEnum, IsMongoId, IsOptional, Length } from 'class-validator';
+import xss from 'xss';
 
 export class PostDTO {
   @IsMongoId()
   id: string;
 
   @Length(1, 280)
+  @Transform(({ value }) => xss(value))
   content: string;
 
   @IsEnum(PostCategory)
