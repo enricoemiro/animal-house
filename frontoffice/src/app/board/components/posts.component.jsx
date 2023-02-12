d8683c8556fd6d2ba76bbaa21060dfd66bf4f3c8
Enrico Emiro <16721212+enricoemiro@users.noreply.github.com>
2023-02-12 01:12:46 +0100

    chore: add react-smart-masonry + refactor Posts

--- frontoffice/src/app/board/components/posts.component.jsx
+++ frontoffice/src/app/board/components/posts.component.jsx
@@ -1,6 +1,7 @@
-import { Box, Grid, Text } from '@mantine/core';
+import { Flex, Text, useMantineTheme } from '@mantine/core';
 import { useQuery } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
+import Masonry from 'react-smart-masonry';
 
 import { GET_POSTS, getPosts } from '@/app/board/api/get-posts.api';
 import { CursorPagination } from '@/components/layouts/cursor-pagination.component';
@@ -8,55 +9,49 @@ import { CursorPagination } from '@/components/layouts/cursor-pagination.compone
 import { PostCard } from './post-card.component';
 
 export const Posts = () => {
+  const theme = useMantineTheme();
   const [cursor, setCursor] = useState({});
 
-  const mutation = useQuery({
+  const {
+    data: { posts, meta },
+    refetch,
+  } = useQuery({
     queryKey: [GET_POSTS],
     queryFn: () => getPosts(cursor),
+    initialData: { posts: [], meta: {} },
     retry: false,
   });
 
   useEffect(() => {
-    mutation.refetch();
+    refetch();
   }, [cursor]);
 
-  if (mutation.isLoading) {
-    return <></>;
-  }
-
   return (
-    <>
-      <Box mb="lg" sx={{ display: 'flex', flexDirection: 'column' }}>
-        {mutation.data?.posts?.length > 0 ? (
-          <Grid justify="center">
-            {mutation.data?.posts.map((post) => {
-              return (
-                <Grid.Col span={12} key={post.id} sm={6} md={4}>
-                  <PostCard post={post} />
-                </Grid.Col>
-              );
-            })}
-          </Grid>
-        ) : (
+    <Flex direction="column">
+      <Flex direction="column" sx={(theme) => ({ marginBottom: theme.spacing.md })}>
+        {posts.length === 0 && (
           <Text align="center">It looks like nobody has written anything yet. Be the first!</Text>
         )}
-      </Box>
 
-      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-        <CursorPagination
-          onPreviousPage={() =>
-            setCursor({
-              before: mutation.data?.meta.startCursor,
-            })
-          }
-          onNextPage={() =>
-            setCursor({
-              after: mutation.data?.meta.endCursor,
-            })
-          }
-          {...mutation.data.meta}
-        />
-      </Box>
-    </>
+        {posts.length > 0 && (
+          <Masonry
+            autoArrange={true}
+            breakpoints={{ xs: theme.breakpoints.xs, md: theme.breakpoints.md }}
+            columns={{ xs: 1, md: 3 }}
+            gap={theme.spacing.md}
+          >
+            {posts.map((post) => {
+              return <PostCard key={post.id} post={post} />;
+            })}
+          </Masonry>
+        )}
+      </Flex>
+
+      <CursorPagination
+        onPreviousPage={() => setCursor({ before: meta?.startCursor })}
+        onNextPage={() => setCursor({ after: meta?.endCursor })}
+        {...meta}
+      />
+    </Flex>
   );
 };
