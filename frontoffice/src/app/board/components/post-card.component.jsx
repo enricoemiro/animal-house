06fbcda6a984c0fe49e8d9f6324307c9bedff21f
Enrico Emiro <16721212+enricoemiro@users.noreply.github.com>
2023-02-13 13:42:05 +0100

    chore: add ImageViewer component

--- frontoffice/src/app/board/components/post-card.component.jsx
+++ frontoffice/src/app/board/components/post-card.component.jsx
@@ -1,12 +1,10 @@
-import { Carousel } from '@mantine/carousel';
-import { Box, Group, Image, Paper, Text } from '@mantine/core';
+import { Box, Group, Paper, Text } from '@mantine/core';
 import { Badge } from '@mantine/core';
 import { format } from 'date-fns';
-import uuid from 'react-uuid';
 
 import { useAuth } from '@/app/auth/use-auth.hook';
 import { Avatar } from '@/components/avatar.component';
-import { bufferToBase64 } from '@/utils/image';
+import { ImageViewer } from '@/components/layouts/image-viewer.component';
 
 import { DeletePostButton } from './delete-post-button.component';
 
@@ -43,28 +41,8 @@ export const PostCard = ({ post }) => {
           gap: theme.spacing.xs,
         })}
       >
-        {post.images?.length > 0 && (
-          <Carousel
-            mx="auto"
-            loop
-            withControls={post.images.length > 1}
-            withIndicators={post.images.length > 1}
-          >
-            {post.images.map((image) => {
-              return (
-                <Carousel.Slide key={uuid()}>
-                  <Image
-                    radius="md"
-                    src={`data:image/png;base64,` + bufferToBase64(image.content.data)}
-                  />
-                </Carousel.Slide>
-              );
-            })}
-          </Carousel>
-        )}
-
+        {post.images?.length > 0 && <ImageViewer images={post.images} />}
         <Text size="sm">{post.content}</Text>
-
         {post.category !== null && <Badge>{post.category}</Badge>}
       </Box>
     </Paper>
