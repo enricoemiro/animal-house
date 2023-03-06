34adc46b5b1680127823efac1e755f529f62ef1c
Enrico Emiro <16721212+enricoemiro@users.noreply.github.com>
2023-03-06 20:04:54 +0100

    chore(frontoffice): now the logo redirects to the homepage instead of /frontoffice

--- frontoffice/src/components/layouts/header/header-logo.component.jsx
+++ frontoffice/src/components/layouts/header/header-logo.component.jsx
@@ -1,18 +1,9 @@
 import { Anchor, Title } from '@mantine/core';
-import { useNavigate } from 'react-router-dom';
 
 export const HeaderLogo = () => {
-  const navigate = useNavigate();
-
   return (
     <Title size="h3">
-      <Anchor
-        variant="text"
-        component="a"
-        color="dark"
-        onClick={() => navigate('/')}
-        sx={{ fontFamily: 'Pacifico' }}
-      >
+      <Anchor variant="text" component="a" color="dark" href="/" sx={{ fontFamily: 'Pacifico' }}>
         Animal House
       </Anchor>
     </Title>
