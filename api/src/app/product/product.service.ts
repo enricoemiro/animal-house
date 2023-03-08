60a91277214928311ec9bc16c47badacebc30c4b
Giulio Billi <72358159+giuliobilli01@users.noreply.github.com>
2023-03-08 14:28:24 +0100

    chore(api): add admin product routes

--- api/src/app/product/product.service.ts
+++ api/src/app/product/product.service.ts
@@ -94,4 +94,64 @@ export class ProductService {
       throw error;
     }
   }
+
+  async getProducts() {
+    try {
+      return await this.prismaService.client.product.findMany({
+        select: {
+          id: true,
+          name: true,
+          description: true,
+          availability: true,
+          price: true,
+          images: true,
+          category: true,
+        },
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async editProduct(
+    id: Product['id'],
+    data: Partial<Pick<Product, 'name' | 'description' | 'availability' | 'price' | 'categoryId'>>,
+  ) {
+    try {
+      if (Object.keys(data).length === 0) {
+        return null;
+      }
+
+      return await this.prismaService.client.product.update({
+        where: { id },
+        data,
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteProduct(id: Product['id']) {
+    try {
+      return this.prismaService.client.product.delete({
+        where: { id },
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteProducts(productIDs: Product['id'][]) {
+    try {
+      return this.prismaService.client.product.deleteMany({
+        where: {
+          id: {
+            in: productIDs,
+          },
+        },
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
 }
