6f7c07e9abf7ab200e865f524e57320b86c1b67f
Enrico Emiro <16721212+enricoemiro@users.noreply.github.com>
2023-03-09 11:32:23 +0100

    Merge pull request (#24)

--- frontoffice/src/main.jsx
+++ frontoffice/src/main.jsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client';
 import '@fontsource/pacifico';
 import 'leaflet/dist/leaflet.css';
 
+import L from 'leaflet';
+import iconRetina from 'leaflet/dist/images/marker-icon-2x.png';
+import iconMarker from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
 import { App } from './App';
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: iconRetina,
+  iconUrl: iconMarker,
+  shadowUrl: iconShadow,
+});
 
 createRoot(document.getElementById('root')).render(<App />);
