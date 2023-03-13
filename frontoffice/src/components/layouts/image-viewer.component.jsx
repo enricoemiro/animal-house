fc0025a494baf78c754189cae9524f0cbc0e2f5c
Enrico Emiro <16721212+enricoemiro@users.noreply.github.com>
2023-03-13 19:56:43 +0100

    chore: maybe fixed

--- frontoffice/src/components/layouts/image-viewer.component.jsx
+++ frontoffice/src/components/layouts/image-viewer.component.jsx
@@ -4,21 +4,25 @@ import { useId } from 'react';
 
 import { bufferToBase64 } from '@/utils/image';
 
+const Image = ({image}) => {
+  const id = useId();
+
+  return (
+    <Carousel.Slide key={id}>
+      <Image
+        radius="md"
+        src={`data:image/png;base64,` + bufferToBase64(image.content.data)}
+        alt=""
+      />
+    </Carousel.Slide>
+  );
+}
+
 export const ImageViewer = ({ images }) => {
   return (
     <Carousel mx="auto" loop withControls={images.length > 1} withIndicators={images.length > 1}>
       {images.map((image) => {
-        const id = useId();
-
-        return (
-          <Carousel.Slide key={id}>
-            <Image
-              radius="md"
-              src={`data:image/png;base64,` + bufferToBase64(image.content.data)}
-              alt=""
-            />
-          </Carousel.Slide>
-        );
+        return <Image image={image}/>
       })}
     </Carousel>
   );
